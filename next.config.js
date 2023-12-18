4c8df02f91ad4b9db0073bd08a87dd7f16d822dc
bpdemo2030 <bpdemo2030@gmail.com>
2023-12-18 21:47:55 +0100

    added lock file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/bpsignup',
+  
+  }
+  
+  module.exports = nextConfig
